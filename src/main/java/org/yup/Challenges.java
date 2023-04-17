8cd6b3ec9e0466897a3c51e740e62546b0db45a2
Berny Fontil <Berny.Fontil@gmail.com>
2023-04-17 15:59:45 -0400

    added several challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,7 +1,69 @@
 package org.yup;
-
+import java.util.Scanner;
 
 public class Challenges {
 
-    
+    public String greeting(String name, int age){
+        // let folks into the party if they are 21 and up
+        // politely tell them they cant enter underage
+        if(age >= 21){
+            return "Hello " + name + ", welcome to the party";
+            // this is what happened if the condition is true//
+        }else{
+            // this is what happened if the condition is false
+            return "Hello" + name + ", I am sorry but we cannot let you into the party";
+        }
+
+
+    }
+
+    public String greetingWithUserInput(){
+
+        Scanner scanner = new Scanner(System.in);
+        // we need to ask the user what their name is
+        System.out.print("Enter your Name: ");
+        String name = scanner.nextLine();
+
+        // we need to ask the user what their age is
+        System.out.print("Enter your Age: ");
+        int age = scanner.nextInt();
+        // let folks into the party if they are 21 and up
+        // politely tell them they cant enter underage
+        if(age >= 21){
+            return "Hello " + name + ", welcome to the party";
+            // this is what happened if the condition is true//
+        }else{
+            // this is what happened if the condition is false
+            return "Hello" + name + ", I am sorry but we cannot let you into the party";
+        }
+
+
+    }
+
+    public int addTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int number1 = scanner.nextInt();
+        System.out.print("Enter second number : ");
+        int number2 = scanner.nextInt();
+        return number1 + number2;
+    }
+    public int subtractTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int number1 = scanner.nextInt();
+        System.out.print("Enter second number : ");
+        int number2 = scanner.nextInt();
+        return number1 - number2;
+    }
+    public int multipleTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int number1 = scanner.nextInt();
+        System.out.print("Enter second number : ");
+        int number2 = scanner.nextInt();
+        return number1 * number2;
+    }
+
+
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,11 +1,26 @@
 package org.yup;
+//Doing this to show you an Import, but it is not required because its in the same package//
+
 
 public class JavaChallengesApp {
 
+
     public static void main(String[] args) {
 
-        System.out.println("Hello from the Java Challenges App");
+        // create a new instance of the challenges class so we can use the challenges in our application //
+        Challenges challenges = new Challenges();
+
+        // this runs the greeting method from our challenges and return a string greeting and
+        // stores it in the greeting variable we created//
+        //String greeting = challenges.greeting("Berny", 27);
+
+        // this print the greeting to the console//
+        System.out.println(challenges.subtractTwoNumbers());
+
+
+
 
     }
+
 
 }
